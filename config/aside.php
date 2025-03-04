1d14877b5835ce499fcad266756bf740b6a495c5
EnxhiHoxhaj <hoxhajenxhi1@gmail.com>
2025-03-04 14:51:15 +0100

    add aside bar first step

---
+++ config/aside.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'side_bar' => [
+        [
+            'menu_text' => 'HOME',
+            'route_name' => '',
+            'menu_icon' => '<i class="fa-solid fa-house"></i>',
+        ],
+    ],
+];
